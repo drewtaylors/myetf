4d45a4c0d258413fd496f7c16cd6631f4afbd265
Drew Taylor <drewtaylor888@gmail.com>
2019-09-13 11:37:34 -0700

    should be last of indentation isues

--- src/controllers/fund.controller.js
+++ src/controllers/fund.controller.js
@@ -1,51 +1,51 @@
 import Fund from '../models/fund.model';
 
 const list = (req, res) => {
-	Fund
-		.find()
+  Fund
+    .find()
     .then(funds => res.json(funds))
-		.catch(err => res.status(500).json(err));
+    .catch(err => res.status(500).json(err));
 };
 
 const create = (req, res) => {
-	Fund
-		.create({ ...req.body })
-		.then(fund => res.status(201).json(fund))
-		.catch(err => res.status(400).json(err));
+  Fund
+    .create({ ...req.body })
+    .then(fund => res.status(201).json(fund))
+    .catch(err => res.status(400).json(err));
 };
 
 const detail = (req, res) => {
-	Fund
-		.findOne({ ticker: req.params.ticker })
-		.then(fund => fund ? res.json(fund) : res.status(404).send())
-		.catch(err => res.status(500).send(err));
+  Fund
+    .findOne({ ticker: req.params.ticker })
+    .then(fund => fund ? res.json(fund) : res.status(404).send())
+    .catch(err => res.status(500).send(err));
 };
 
 const update = (req, res) => {
-	Fund
-		.findOneAndUpdate({ 
-			ticker: req.params.ticker
-		}, {
-			...req.body
-		}, {
-			new: true,
-			omitUndefined: true
-		})
-		.then(fund => fund ? res.send(fund) : res.status(404).send())
-		.catch(err => res.status(500).send(err));
+  Fund
+    .findOneAndUpdate({ 
+      ticker: req.params.ticker
+    }, {
+      ...req.body
+    }, {
+      new: true,
+      omitUndefined: true
+    })
+    .then(fund => fund ? res.send(fund) : res.status(404).send())
+    .catch(err => res.status(500).send(err));
 }
 
 const remove = (req, res) => {
-	Fund
-		.findOneAndDelete({ ticker: req.params.ticker })
-		.then(fund => fund ? res.send(fund) : res.status(404).send())
-		.catch(err => res.status(500).send(err));
+  Fund
+    .findOneAndDelete({ ticker: req.params.ticker })
+    .then(fund => fund ? res.send(fund) : res.status(404).send())
+    .catch(err => res.status(500).send(err));
 }
 
 export {
-	list,
-	create,
-	detail,
-	update,
-	remove
+  list,
+  create,
+  detail,
+  update,
+  remove
 };
--- src/controllers/stock.controller.js
+++ src/controllers/stock.controller.js
@@ -1,50 +1,50 @@
 import Stock from '../models/stock.model';
 
 const list = (req, res) => {
-	Stock
-		.find()
+  Stock
+    .find()
     .then(stocks => res.json(stocks))
-		.catch(err => res.status(500).json(err));
+    .catch(err => res.status(500).json(err));
 };
 
 const create = (req, res) => {
-	Stock
-		.create({ ...req.body })
-		.then(fund => res.status(201).json(fund))
-		.catch(err => res.status(400).json(err));
+  Stock
+    .create({ ...req.body })
+    .then(fund => res.status(201).json(fund))
+    .catch(err => res.status(400).json(err));
 };
 
 const detail = (req, res) => {
-	Stock
-		.findOne({ ticker: req.params.ticker })
-		.then(stock => stock ? res.json(stock) : res.status(404).send())
+  Stock
+    .findOne({ ticker: req.params.ticker })
+    .then(stock => stock ? res.json(stock) : res.status(404).send())
 }
 
 const update = (req, res) => {
-	Stock
-		.findOneAndUpdate({
-			ticker: req.params.ticker
-		}, {
-			...req.body
-		}, {
-			new: true,
-			omitUndefined: true
-		})
-		.then(stock => stock ? res.send(stock) : res.status(404).send())
-		.catch(err => res.status(500).send(err));
+  Stock
+    .findOneAndUpdate({
+      ticker: req.params.ticker
+    }, {
+      ...req.body
+    }, {
+      new: true,
+      omitUndefined: true
+    })
+    .then(stock => stock ? res.send(stock) : res.status(404).send())
+    .catch(err => res.status(500).send(err));
 }
 
 const remove = (req, res) => {
-	Stock
-		.findOneAndDelete({ ticker: req.params.ticker })
-		.then(stock => stock ? res.send(stock) : res.status(404).send())
-		.catch(err => res.status(500).send(err));
+  Stock
+    .findOneAndDelete({ ticker: req.params.ticker })
+    .then(stock => stock ? res.send(stock) : res.status(404).send())
+    .catch(err => res.status(500).send(err));
 }
 
 export {
   list,
-	create,
-	detail,
-	update,
-	remove
+  create,
+  detail,
+  update,
+  remove
 };
